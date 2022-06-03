e27a59c596fe2bcf8fad8cdd3c343b8f3a63724a
harshvardhan singh <harshvardhansingh458@gmail.com>
2022-06-03 17:19:39 +0530

    Completed this static page about react

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav className="nav-bar">
+            <img src="../images/react-icon.png" alt="" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default NavBar
